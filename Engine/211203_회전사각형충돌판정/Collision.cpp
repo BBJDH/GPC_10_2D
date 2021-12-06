92224284343a120608c8b90f3c5abe6ec8487f3a
PL_3D_010 <PL_3D_010@DESKTOP-H77UJ7K>
2021-12-06 11:57:12 +0900

    도형 충돌 과제

--- Engine/211203_회전사각형충돌판정/Collision.cpp
+++ Engine/211203_회전사각형충돌판정/Collision.cpp
@@ -1,6 +1,16 @@
 #include "vector.h"
-#include <windef.h>
 #include <vector>
+
+struct Point final
+{   
+    vector<2> Location = { 0,0 };
+};
+
+struct Circle final
+{
+    float Diameter = { 0 };
+    vector<2> Location = { 0,0 };
+};
 
 struct  Rectangle
 {
@@ -8,6 +18,40 @@ struct  Rectangle
     float     Angle = { 0 };
     vector<2> Location = {0,0};
 };
+
+bool Collide(Point const& dest, Circle const& src)
+{
+
+    return length(dest.Location - src.Location) <= (src.Diameter / 2);
+}
+bool Collide(Circle const& dest, Circle const& src)
+{
+
+    return length(dest.Location - src.Location) <= (dest.Diameter / 2 + src.Diameter / 2);
+}
+bool Collide(Point const& dest, Rectangle const& src)
+{
+    vector<2> const min = { src.Location[0] - src.Length[0],src.Location[1] - src.Length[1] };
+    vector<2> const max = { src.Location[0] + src.Length[0],src.Location[1] + src.Length[1] };
+
+    return (min[0] <= dest.Location[0]) && (dest.Location[0] <= max[0]) && ((min[1] <= dest.Location[1]) && (dest.Location[1] <= max[1]));
+}
+bool Collide(Point const& dest, Rectangle const& src)
+{
+    vector<2> const min = { src.Location[0] - src.Length[0],src.Location[1] - src.Length[1] };
+    vector<2> const max = { src.Location[0] + src.Length[0],src.Location[1] + src.Length[1] };
+
+    return (min[0] <= dest.Location[0]) && (dest.Location[0] <= max[0]) && ((min[1] <= dest.Location[1]) && (dest.Location[1] <= max[1]));
+}
+bool Collide(Rectangle const& dest, Rectangle const& src)
+{
+    vector<2> const sum_length = { dest.Length[0] + src.Length[0],dest.Length[1] + src.Length[1] };
+    //사각형의 너비 합과 높이 합보다 x간격과 y간격이 더 작으면 충돌
+
+    return ((dest.Location[0]+src.Location[0])<=sum_length[0])&& ((dest.Location[1] + src.Location[1]) <= sum_length[1]);
+}
+//사각형 사각형
+//원 사각형
 
 bool Collide(Rectangle const& LHS, Rectangle const& RHS)
 {
